27de9c04f8793c9d97f569d0a4b997d1aed88bd1
YounSoyoung <63245740+YounSoyoung@users.noreply.github.com>
2022-04-12 19:04:23 +0900

    Create yearMonth.sql

---
+++ yearMonth.sql
@@ -0,0 +1,18 @@
+INSERT INTO statistc.requestInfo(requestID, requestCode, userID, createDate )
+VALUES(5, 'O', 'AAA', '2008180710'),
+(6, 'O', 'EEE', '2004150204'),
+(7, 'L', 'GGG', '2007020912'),
+(8, 'L', 'HHH', '2007020815');
+
+CREATE table statistc.month(
+SELECT SUBSTR(createDate, 1, 4) AS yearMonth, userID AS userID
+FROM statistc.requestInfo
+GROUP BY createDate
+ORDER BY createDate ASC
+);
+
+SELECT yearMonth AS 년월,
+		COUNT(DISTINCT userID) AS 접속자수
+FROM statistc.month
+GROUP BY yearMonth
+ORDER BY yearMonth ASC;
