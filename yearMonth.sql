6f5f8ade77e735eef2f148df5feea0a6d39e7b80
YounSoyoung <63245740+YounSoyoung@users.noreply.github.com>
2022-04-18 18:24:01 +0900

    Update yearMonth.sql

--- yearMonth.sql
+++ yearMonth.sql
@@ -4,15 +4,16 @@ VALUES(5, 'O', 'AAA', '2008180710'),
 (7, 'L', 'GGG', '2007020912'),
 (8, 'L', 'HHH', '2007020815');
 
-CREATE table statistc.month(
-SELECT SUBSTR(createDate, 1, 4) AS yearMonth, userID AS userID
-FROM statistc.requestInfo
-GROUP BY createDate
-ORDER BY createDate ASC
-);
-
 SELECT yearMonth AS 년월,
-	COUNT(DISTINCT userID) AS 접속자수
-FROM statistc.month
+		COUNT(DISTINCT userID) AS 접속자수
+FROM(
+SELECT SUBSTR(createDate, 1, 4) AS yearMonth,
+		userID AS userID
+FROM statistc.requestInfo) AS monthLogin
 GROUP BY yearMonth
 ORDER BY yearMonth ASC;
+
+
+
+
+
