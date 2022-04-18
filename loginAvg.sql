0876cf5bbe3b7bf4dc7b95d64b1b12e8c795c633
YounSoyoung <63245740+YounSoyoung@users.noreply.github.com>
2022-04-18 16:05:07 +0900

    Update loginAvg.sql

--- loginAvg.sql
+++ loginAvg.sql
@@ -4,19 +4,17 @@ VALUES(9, 'L', 'DDD', '2004040305'),
 (11, 'L', 'XXX', '2004040920'),
 (12, 'L', 'ZZZ', '2008180421');
 
-CREATE table statistc.login(
-SELECT userID AS userID, 
-SUBSTR(createDate, 1, 6) AS loginDate
-FROM statistc.requestInfo WHERE requestCode = "L"
-GROUP BY createDate
-ORDER BY createDate ASC
-);
+WITH `login` AS (
+SELECT userID AS userID,
+	SUBSTR(createDate, 1, 6) AS loginDate
+FROM statistc.requestInfo WHERE requestCode = 'L'
+)
 
-CREATE table statistc.loginCnt(
-SELECT loginDate as loginDate,
-	COUNT(DISTINCT userID) AS loginCount
-FROM statistc.login
-GROUP BY loginDate
-ORDER BY loginDate ASC);
+SELECT avg(loginCnt.loginCount) AS 하루평균로그인횟수
+FROM(SELECT loginDate AS loginDate,
+			COUNT(DISTINCT userID) AS loginCount
+            FROM `login`
+            GROUP BY loginDate
+            ORDER BY loginDate ASC) AS loginCnt;
 
-SELECT AVG(loginCount) FROM statistc.loginCnt;
+
