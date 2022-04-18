1e219e2ee78092e5cdf7a0685113628aa419cb76
YounSoyoung <63245740+YounSoyoung@users.noreply.github.com>
2022-04-18 18:06:31 +0900

    Update organLogin.sql

--- organLogin.sql
+++ organLogin.sql
@@ -9,17 +9,16 @@ VALUES('AAA', 'B', 'Tom'),
 ('XXX', 'C', 'Lee'),
 ('ZZZ', 'A', 'Sung');
 
-CREATE table statistc.organLogin (
-SELECT SUBSTR(createDate, 1, 4) AS createDate,
-    userID AS userID,
-    requestCode AS requestCode
-FROM statistc.requestInfo
-WHERE userID IN (SELECT userID FROM statistc.user WHERE HR_ORGAN = 'A')
-GROUP BY createDate
-ORDER BY createDate ASC
-);
-
-SELECT createDate AS LoginDate,
-		COUNT(DISTINCT userID) AS LoginCnt
-FROM statistc.organLogin
-WHERE createDate = 2006 AND requestCode = 'L';
+SELECT LoginDate AS LoginDate,
+		COUNT(DISTINCT userID) AS loginCnt,
+        HR_ORGAN AS HR_ORGAN
+FROM (
+SELECT SUBSTR(createDate, 1, 4) AS LoginDate,
+		userID AS userID,
+        requestCode AS requestCode,
+        HR_ORGAN as HR_ORGAN
+FROM (SELECT sr.createDate, sr.userID, sr.requestCode, su.HR_ORGAN
+FROM statistc.requestInfo AS sr INNER JOIN statistc.user AS su
+ON sr.userID = su.userID) AS organLogin) AS organ
+GROUP BY HR_ORGAN
+HAVING HR_ORGAN = 'A' AND LoginDate = 2006;
